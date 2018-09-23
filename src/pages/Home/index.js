e853cac26fb4f0568cf07ef418d73303ba6f2450
iputra <ikhsannotpa@gmail.com>
2018-09-23 15:30:59 +0700

    Add material ui kit

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,78 @@
 import React from "react";
 // import Button from "../../components/Button"
 // import Status from "../../components/Status"
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import Input from '@material-ui/core/Input';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import { withStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
 
 const link = "https://cdn.rawgit.com/kevinhermawan/ca5e0083648ba5ffb2421808d972dd9c/raw/c29c7ee02849b58024fb6a058acae33bde38cbd3/react-blog-example.json";
+
+const styles = theme => ({
+  root: {
+	width: '100%',
+  },
+  grow: {
+	flexGrow: 1,
+  },
+  menuButton: {
+	marginLeft: -12,
+	marginRight: 20,
+  },
+  title: {
+	display: 'none',
+	[theme.breakpoints.up('sm')]: {
+	  display: 'block',
+	},
+  },
+  search: {
+	position: 'relative',
+	borderRadius: theme.shape.borderRadius,
+	backgroundColor: fade(theme.palette.common.white, 0.15),
+	'&:hover': {
+	  backgroundColor: fade(theme.palette.common.white, 0.25),
+	},
+	marginLeft: 0,
+	width: '100%',
+	[theme.breakpoints.up('sm')]: {
+	  marginLeft: theme.spacing.unit,
+	  width: 'auto',
+	},
+  },
+  searchIcon: {
+	width: theme.spacing.unit * 9,
+	height: '100%',
+	position: 'absolute',
+	pointerEvents: 'none',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+  },
+  inputRoot: {
+	color: 'inherit',
+	width: '100%',
+  },
+  inputInput: {
+	paddingTop: theme.spacing.unit,
+	paddingRight: theme.spacing.unit,
+	paddingBottom: theme.spacing.unit,
+	paddingLeft: theme.spacing.unit * 10,
+	transition: theme.transitions.create('width'),
+	width: '100%',
+	[theme.breakpoints.up('sm')]: {
+	  width: 120,
+	  '&:focus': {
+		width: 200,
+	  },
+	},
+  },
+});
 
 class Home extends React.Component {
 	constructor(props){
@@ -49,12 +119,39 @@ class Home extends React.Component {
 			width: 800,
 			marginLeft: 240
 		}
+
+
+		const { classes } = this.props;
+
 		return (
 			<div>
-				<input
-					style={{marginLeft: 240, marginTop: 34}}
-					onChange={this.handleForm}
-					value={this.state.keyword} />
+				<AppBar position="static">
+					<Toolbar>
+					  <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
+						<MenuIcon />
+					  </IconButton>
+					  <Typography className={classes.title} variant="title" color="inherit" noWrap>
+						Material-UI
+					  </Typography>
+					  <div className={classes.grow} />
+					  <div className={classes.search}>
+						<div className={classes.searchIcon}>
+						  <SearchIcon />
+						</div>
+						<Input
+						  placeholder="Search…"
+						  disableUnderline
+						  classes={{
+							root: classes.inputRoot,
+							input: classes.inputInput,
+						  }}
+							onChange={this.handleForm}
+							value={this.state.keyword}
+						/>
+					  </div>
+					</Toolbar>
+				  </AppBar>
+
 
 				{this.state.list &&
 					this.state.list
@@ -82,6 +179,10 @@ class Home extends React.Component {
 	};
 };
 
-export default Home;
+Home.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
+export default withStyles(styles)(Home);
 
+// export default Home;
